b5a21ad5ef3768fa153a4d3400654c2c7d42b298
Francky <gwadafrank@gmail.com>
2022-02-18 19:43:42 +0300

    implement object function that can add and delete properties

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    'name':"frank",
+    "streetAdress": "123"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmp = {...employee}
+    newEmp[key] = value
+    // console.log("newEmp");
+    return newEmp
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){ 
+    employee[key]=value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+
+    const newEmp = {...employee}
+
+    delete newEmp[key]
+    return newEmp
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+
+    return employee
+
+}
